cd945555105e925cc7580434f813095dcbd61165
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2022-04-04 09:04:31 -0400

    init blazor blog

---
+++ Data/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace blazor_blog_2022.Data;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
